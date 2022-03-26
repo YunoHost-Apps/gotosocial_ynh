b7487729bf79b30bed7439370a907a33a3cf0dbd
OniriCorpe <oniricorpe@disroot.org>
2022-03-26 05:35:44 +0100

    $1 double quoted

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -33,7 +33,7 @@ detect_arch(){
 
 # custom function to change bash bool 0/1 to false/true
 convert_bool(){
-	(($1)) && echo "true" || echo "false"
+	(("$1")) && echo "true" || echo "false"
 }
 
 #=================================================
