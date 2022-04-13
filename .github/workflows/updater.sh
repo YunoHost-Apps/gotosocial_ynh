91cb59e1c0d5380883cb893e026487e6dc8d9251
OniriCorpe <oniricorpe@disroot.org>
2022-04-13 20:29:23 +0200

    fix

--- .github/workflows/updater.sh
+++ .github/workflows/updater.sh
@@ -17,8 +17,7 @@
 #=================================================
 
 # Fetching information
-# current_version=$(cat manifest.json | jq -j '.version|split("~")[0]')
-current_version="0.2.0"
+current_version=$(cat manifest.json | jq -j '.version|split("~")[0]')
 repo=$(cat manifest.json | jq -j '.upstream.code|split("https://github.com/")[1]')
 # Some jq magic is needed, because the latest upstream release is not always the latest version (e.g. security patches for older versions)
 version=$(curl --silent "https://api.github.com/repos/$repo/releases" | jq -r '.[] | select( .prerelease != true ) | .tag_name' | sort -V | tail -1)
