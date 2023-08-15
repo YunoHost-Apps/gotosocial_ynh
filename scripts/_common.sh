9e31857051cae3eba58f74c32297e586f39cf45b
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-08-15 17:19:52 +0200

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,26 +8,6 @@
 # PERSONAL HELPERS
 #=================================================
 
-# custom function to detect armv6 and armv7
-# ($YNH_ARCH returns armhf for both...)
-detect_arch(){
-	local architecture
-	if [ -n "$(uname -m | grep arm64)" ] || [ -n "$(uname -m | grep aarch64)" ]; then
-		architecture="arm64"
-	elif [ -n "$(uname -m | grep 64)" ]; then
-		architecture="x86-64"
-	elif [ -n "$(uname -m | grep 86)" ]; then
-		architecture="i386"
-	elif [ -n "$(uname -m | grep armv6)" ]; then
-		architecture="armv6"
-	elif [ -n "$(uname -m | grep armv7)" ]; then
-		architecture="armv7"
-	else
-		architecture="unknown"
-	fi
-	echo $architecture
-}
-
 # custom function to change bash bool 0/1 to false/true
 convert_bool(){
 	(("$1")) && echo "true" || echo "false"
