d56c0428df4a0a3e4c72f38515b19a12bad7afc5
OniriCorpe <oniricorpe@disroot.org>
2025-03-24 19:51:14 +0100

    32 bits system must use the nowasm builds too

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -15,6 +15,10 @@ nowasm_detection(){
 	if grep -qE '^flags.* (sse4|LSE)' /proc/cpuinfo; then
 		# Supported system; using the standard builds
 		build_version="main"
+	elif ! [ $(getconf LONG_BIT) -eq 64 ]; then
+		# Non 64 bits system; using the 'nowasm' builds
+		ynh_print_warn "Your system is 32 bits, so doesn't support the standard GoToSocial builds. The package will install the *experimental and unsupported* 'nowasm' builds. More info: https://docs.gotosocial.org/en/latest/advanced/builds/nowasm/"
+		build_version="nowasm"
 	else
 		# Non-supported system; using the 'nowasm' builds
 		ynh_print_warn "Your system doesn't support the standard GoToSocial builds. So the package will install the *experimental and unsupported* 'nowasm' builds. More info: https://docs.gotosocial.org/en/latest/advanced/builds/nowasm/"
